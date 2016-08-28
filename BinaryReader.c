5d4e3ccb9901c0cc98f22c60d97830dcd6d58718
Brian Kimmel <kimmelb@gmail.com>
2016-08-28 18:25:48 -0400

    Starting to write binary string converter

--- BinaryReader.c
+++ BinaryReader.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+
+char *ByteToBinary(unsigned char inputByte)
+{
+	int i = inputByte;
+	
+	int bitplace = 128;
+
+	char stringplace = "0";
+	
+	while(bitplace >= 1)
+	{
+		if( i > bitplace )
+		{
+			i = i - bitplace;
+			stringplace = "1";
+		}
+		else
+			stringPlace = "0";
+		bitplace = bitplace/2;
+	}
+}
+
 int main(void)
 {
   printf("Hello, world!\n");

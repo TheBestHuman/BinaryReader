41834eacef425c0c601330659520f4942b7250cc
Brian Kimmel <kimmelb@gmail.com>
2016-08-28 19:41:27 -0400

    reading a file and oute and outputting char by char

--- BinaryReader.c
+++ BinaryReader.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
 
-char *ByteToBinary(unsigned char inputByte)
+//char *ByteToBinary(unsigned char inputByte)
+void ByteToBinary(unsigned char inputByte)
 {
 	int i = inputByte;
 	
 	int bitplace = 128;
 
-	char stringplace = "0";
+	char stringplace = '0';
 	
 	while(bitplace >= 1)
 	{
 		if( i > bitplace )
 		{
 			i = i - bitplace;
-			stringplace = "1";
+			stringplace = '1';
 		}
 		else
-			stringPlace = "0";
+			stringplace = '0';
 		bitplace = bitplace/2;
+		printf("%c",stringplace);
 	}
+	
 }
 
 int main(void)
 {
-  printf("Hello, world!\n");
-  return 0;
+	unsigned char buff[255];
+	FILE *filestream;
+	int bytesread = 0;
+
+	filestream = fopen("firstfile.bin", "rb");
+	while(!feof(filestream))
+	{
+		bytesread = fread(buff, 1, sizeof(buff), filestream);
+		printf("(%d)", bytesread);
+		for(int i = 0; i < bytesread; i++)
+		{
+			printf("%c", buff[i]);
+			//printf("%c: ", buff[i]);
+			//ByteToBinary(buff[i]);
+			printf(" ");
+		}
+	}
+  	return 0;
 }

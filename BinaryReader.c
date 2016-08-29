26db46e0c7d03a2a7a5e4802c50845e141f96b42
Brian Kimmel <kimmelb@gmail.com>
2016-08-28 20:53:54 -0400

    fixing return value of the ByteToBinary method

--- BinaryReader.c
+++ BinaryReader.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-//char *ByteToBinary(unsigned char inputByte)
-void ByteToBinary(unsigned char inputByte)
+char *ByteToBinary(unsigned char inputByte)
+//void ByteToBinary(unsigned char inputByte)
 {
 	//printf("%c:", inputByte);
 	int i = inputByte;
 	
 	int bitplace = 128;
 
-	char stringplace = '0';
+	char *stringplace = "0";
+	char retstring[9];
+	retstring[0] = '\0';	
 	
 	while(bitplace >= 1)
 	{
@@ -17,33 +19,37 @@ void ByteToBinary(unsigned char inputByte)
 		if( i >= bitplace )
 		{
 			i = i - bitplace;
-			stringplace = '1';
+			stringplace = "1";
 		}
 		else
-			stringplace = '0';
+			stringplace = "0";
 		bitplace = bitplace/2;
-		printf("%c",stringplace);
+		strcat(retstring, stringplace);
+		printf("%s",stringplace);
 	}
+	char* retval = retstring;
+	return retval;
 	
 }
 
 int main(int argc, char **argv)
 {
 
-	char *filename;
+	char *inputfilename;
 	//printf("%d:  ", argc);
 	if(argc > 1)
-		filename = argv[1];
+		inputfilename = argv[1];
 	else
-		filename = "firstfile.bin";
+		inputfilename = "firstfile.bin";
 
-	//printf("%s", filename);
+	//printf("%s", inputfilename);
 
 	unsigned char buff[255];
 	FILE *filestream;
 	int bytesread = 0;
 
-	filestream = fopen(filename, "rb");
+	char *currentbyte = "\0";
+	filestream = fopen(inputfilename, "rb");
 	while(!feof(filestream))
 	{
 		bytesread = fread(buff, 1, sizeof(buff), filestream);
@@ -51,10 +57,11 @@ int main(int argc, char **argv)
 		for(int i = 0; i < bytesread; i++)
 		{
 			//printf("%c: ", buff[i]);
-			ByteToBinary(buff[i]);
-			printf(" ");
+			currentbyte = ByteToBinary(buff[i]);
+			printf("%s ", currentbyte);
 		}
 	}
+	fclose(filestream);
 	printf("\n");
   	return 0;
 }
